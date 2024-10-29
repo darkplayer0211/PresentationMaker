a6c856c8f8f45242c81ae85e67a580b41fac7462
Mentoriassssssss <pghuy18@gmail.com>
2024-10-29 16:21:24 +0700

    restructure the folder structure for a Node.js project. (#10)
    restructure nodejs
    run example api in nodejs
    api calling example in reactjs
    
    Co-authored-by: Quoctrun <trung147to@gmail.com>

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,1 +1,17 @@
-//The starting point of the application (initalizing Express, middleware)
+// create simple rest API
+//The following code is just for testing how Nodejs works; it is only an example for usafe, and you can delete it.
+
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser'; 
+
+
+import todoRoutes from './routes/todos';
+
+const app = express();
+app.use(json());
+
+app.use('/todos', todoRoutes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message})
+});
+app.listen(3000);
