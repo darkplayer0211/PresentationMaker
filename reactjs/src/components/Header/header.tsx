f26b6915c2d40ef8fcf6007dbfd952f28f2962fe
Tri Nguyen <rinkitorichishiki.it@gmail.com>
2024-11-23 16:39:26 +0700

    Save the current status

--- reactjs/src/components/Header/header.tsx
+++ reactjs/src/components/Header/header.tsx
@@ -1,38 +1,34 @@
-import '../../css/defaultLayout/header/header.css';
+import "../../css/defaultLayout/header/header.css";
 import { RxAvatar } from "react-icons/rx";
-import Sidebar from '../Sidebar/sidebar';
+import Sidebar from "../Sidebar/sidebar";
 
-interface HeaderProps {
-}
+interface HeaderProps {}
 
-const Header : React.FC<HeaderProps> = () => {
+const Header: React.FC<HeaderProps> = () => {
+  const openSidebar = () => {
+    const sidebar = document.querySelector(".sidebar") as HTMLElement;
+    sidebar.classList.add("sidebar_open");
 
-    const openSidebar = () => {
-        const sidebar = document.querySelector('.sidebar') as HTMLElement;
-        sidebar.classList.add('sidebar_open');
+    const sidebarContent = document.querySelector(
+      ".sidebar_content"
+    ) as HTMLElement;
+    sidebarContent.classList.add("sidebar_content_open");
+  };
 
-        const sidebarContent = document.querySelector('.sidebar_content') as HTMLElement;
-        sidebarContent.classList.add('sidebar_content_open');
-    }
-
-    return(
-        <div className="header">
-            <div onClick={openSidebar} className="header_burgerMenu">
-                <div className="header_burgerMenu_line"></div>
-                <div className="header_burgerMenu_line"></div>
-                <div className="header_burgerMenu_line"></div>
-            </div>
-            <div className="header_title">
-                Presentation Maker
-            </div>
-            <div className="header_userInfo">
-                <RxAvatar color='var(--text)'/>
-                <div className="header_userInfo_username">
-                    Username
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div onClick={openSidebar} className="header_burgerMenu">
+        <div className="header_burgerMenu_line"></div>
+        <div className="header_burgerMenu_line"></div>
+        <div className="header_burgerMenu_line"></div>
+      </div>
+      <div className="header_title">Presentation Maker</div>
+      <div className="header_userInfo">
+        <RxAvatar color="var(--text)" />
+        <div className="header_userInfo_username">Username</div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
